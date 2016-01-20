e722a3e8d35ae4eee209ea50685a56d3c047331e
Natcha Luang - Aroonchai <me@nomkhonwaan.com>
2016-01-20 22:23:40 +0700

    Installing unzip if appcfg.py is not exists

--- run.sh
+++ run.sh
@@ -5,17 +5,18 @@ command_exists()
   hash "$1" 2>/dev/null
 }
 
-if ! command_exists unzip; then
-  sudo apt-get update
-  sudo apt-get install -y unzip
-fi
-
 if [ -f "$WERCKER_CACHE_DIR/go_appengine/appcfg.py" ]; then
   debug "appcfg.py found in cache"
 else
   debug "Downloading AppEngine SDK..."
   export FILE=$(curl https://storage.googleapis.com/appengine-sdks/ | grep -o 'featured/go_appengine_sdk_linux_amd64-[^\<]*' | tail -1)
   curl -O https://storage.googleapis.com/appengine-sdks/$FILE
+
+  debug "Installing unzip..."
+  if ! command_exists unzip; then
+    sudo apt-get update
+    sudo apt-get install -y unzip
+  fi
 
   debug "Extracting..."
   unzip -q `echo $FILE | awk -F / '{print $2}'`
